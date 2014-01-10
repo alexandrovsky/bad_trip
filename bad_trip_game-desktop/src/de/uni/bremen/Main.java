4d8314984afc262530d42f3ca178defc2708f8bf
alexandrovsky <dimi@dmitrys-mbp-2.fritz.box>
2014-01-10 19:50:10 +0100

    project added

---
+++ bad_trip_game-desktop/src/de/uni/bremen/Main.java
@@ -0,0 +1,16 @@
+package de.uni.bremen;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "bad_trip_game";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new BadTripGame(), cfg);
+	}
+}
